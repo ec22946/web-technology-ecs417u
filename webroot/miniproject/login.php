5a23f06f68cba23baa55f7edb9949f79dbddf24b
ec22946 <130775810+ec22946@users.noreply.github.com>
2023-04-18 18:00:34 +0100

    Add files via upload

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -18,19 +18,48 @@
 		</hgroup>
 		
 		<?php
-
-				$username = $_POST["username"];
-				$password = $_POST["password"];
-				
+			/**
+			CREATE TABLE USERS (
+			ID int NOT NULL AUTO_INCREMENT,
+			username varchar(255),
+			password varchar(255),
+			PRIMARY KEY (ID)
+			);
+			**/
+			
+			$username = $_POST["username"];
+			$password = $_POST["password"];
+		
+			$dbhost = getenv("MYSQL_SERVICE_HOST");
+			$dbport = getenv("MYSQL_SERVICE_PORT");
+			$dbuser = getenv("DATABASE_USER");
+			$dbpwd = getenv("DATABASE_PASSWORD");
+			$dbname = getenv("DATABASE_NAME");
+			$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+			if ($conn->connect_error)
+			{
+				die("Connection failed: " . $conn->connect_error);
+			}
+			
+			if ($_SERVER['REQUEST_METHOD'] === 'POST')
+			{
+				if ($conn->query($sql) === TRUE)
+				{
+					$sql = "INSERT INTO USERS (username, password) VALUES ("$username", "$password")";
+				}
+				else
+				{
+					echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+				$conn->close();
+			}
 		?>
 		
 		<p>
-			Username: <?php echo $username;?>
+			Username: <?php echo $username; ?>
 		</p>
 		<p>
-			Password: <?php echo $password;?>
+			Password: <?php echo $password; ?>
 		</p>
-		
-
 	</body>
 </html>
