15c11d55fb1016768eead2564744838cfb406036
ec22946 <130775810+ec22946@users.noreply.github.com>
2023-04-18 19:46:47 +0100

    Add files via upload

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -48,8 +48,8 @@
 			{
 				$sql = "SELECT password FROM USERS WHERE username = '$username'";
 				
-				if ($conn->query($sql) === TRUE)
-				{								
+				//if ($conn->query($sql) === TRUE)
+				//{								
 					$retrieved_password = $db->query($sql)->fetch_object()->service_label;
 					
 					echo "<br> $retrieved_password";
@@ -58,13 +58,13 @@
 					{
 						$authenticated = TRUE;
 					}
-				}
-				
+				//}
+				/**
 				else
 				{
 					echo "Error: " . $sql . "<br>" . $conn->error;
 				}
-				
+				**/
 				$conn->close();
 			}
 		?>
