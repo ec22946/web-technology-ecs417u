c918dc341fb6f263c8b0f67b113313196bd975e8
ec22946 <130775810+ec22946@users.noreply.github.com>
2023-04-19 20:31:20 +0100

    Add files via upload

--- webroot/miniproject/addPost.php
+++ webroot/miniproject/addPost.php
@@ -15,8 +15,32 @@
 			$blog_post_title = $_POST["title"];
 			$blog_post_content = $_POST["content"];
 			
-			echo $blog_post_title;
-			echo $blog_post_content;
+			$dbhost = getenv("MYSQL_SERVICE_HOST");
+			$dbport = getenv("MYSQL_SERVICE_PORT");
+			$dbuser = getenv("DATABASE_USER");
+			$dbpwd = getenv("DATABASE_PASSWORD");
+			$dbname = getenv("DATABASE_NAME");
+			// Creates connection
+			$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+			// Checks connection
+			if ($conn->connect_error)
+			{
+				die("Connection failed: " . $conn->connect_error);
+			}
+			
+			if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			$sql = "INSERT INTO BLOG (title, content) VALUES ('$blog_post_title', '$blog_post_content')";
+			if ($conn->query($sql) === TRUE)
+			{
+				
+			}
+			else
+			{
+			echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+			$conn->close();
+			}
+			
 			
 			
 			/**
